52b4bf2c7740ca14d5e5316753d523e1213eeac7
Peter-Nagy <nagypeterszabolcs@gmail.com>
2021-02-08 14:27:29 +0100

    initial commit

---
+++ A/Shared/AApp.swift
@@ -0,0 +1,17 @@
+//
+//  AApp.swift
+//  Shared
+//
+//  Created by Péter Szabolcs Nagy on 2021. 02. 08..
+//
+
+import SwiftUI
+
+@main
+struct AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
